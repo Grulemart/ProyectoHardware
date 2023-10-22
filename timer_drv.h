380b3da941d632301b7e19161049e473d7f0e431
alv02 <120688930+alv02@users.noreply.github.com>
2023-10-22 19:48:58 +0200

    Update timer_drv.h

--- timer_drv.h
+++ timer_drv.h
@@ -5,9 +5,9 @@
 #include <stdint.h>
 #include "timer_hal.h"
 
-void temporizador_drv_iniciar();
-void temporizador_drv_empezar();
-uint64_t temporizador_drv_leer();
-uint64_t temporizador_drv_parar();
+void temporizador_drv_iniciar(void);
+void temporizador_drv_empezar(void);
+uint64_t temporizador_drv_leer(void);
+uint64_t temporizador_drv_parar(void);
 
 #endif
