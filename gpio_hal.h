fa10742728bb40c5bf4c18a42c3b00f45d6d6d7d
alv02 <120688930+alv02@users.noreply.github.com>
2023-10-22 19:49:13 +0200

    Update gpio_hal.h

--- gpio_hal.h
+++ gpio_hal.h
@@ -3,7 +3,7 @@
 
 #include "io_reserva.h"
 
-void gpio_hal_iniciar();
+void gpio_hal_iniciar(void);
 void gpio_hal_sentido(GPIO_HAL_PIN_T gpio_inicial, uint8_t num_bits, gpio_hal_pin_dir_t direccion);
 uint32_t gpio_hal_leer(GPIO_HAL_PIN_T gpio_inicial, uint8_t num_bits);
 void gpio_hal_escribir(GPIO_HAL_PIN_T gpio_inicial, uint8_t num_bits, uint32_t valor);
