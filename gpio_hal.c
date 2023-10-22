2d1f689f12a48d211d239039ebf3d0a90ecda3ed
alv02 <120688930+alv02@users.noreply.github.com>
2023-10-22 19:47:37 +0200

    Update gpio_hal.c

--- gpio_hal.c
+++ gpio_hal.c
@@ -4,12 +4,13 @@
 	
 void gpio_hal_iniciar(){
 	PINSEL0 = 0x00000000;
+	
 }
 
 void gpio_hal_sentido(GPIO_HAL_PIN_T gpio_inicial, uint8_t num_bits, gpio_hal_pin_dir_t direccion){
-	// Verifica que los bits a configurar estén en el rango correcto
+	// Verifica que los bits a configurar estÃ©n en el rango correcto
     if (num_bits > 0 && num_bits <= 32) {
-        // Máscara para los bits a configurar
+        // MÃ¡scara para los bits a configurar
         uint32_t mascara = (1U << num_bits) - 1U;
         
         if (direccion == GPIO_HAL_PIN_DIR_INPUT) {
@@ -23,9 +24,9 @@ void gpio_hal_sentido(GPIO_HAL_PIN_T gpio_inicial, uint8_t num_bits, gpio_hal_pi
 }
 
 uint32_t gpio_hal_leer(GPIO_HAL_PIN_T gpio_inicial, uint8_t num_bits){
-	// Verifica que los bits a leer estén en el rango correcto
+	// Verifica que los bits a leer estÃ©n en el rango correcto
     if (num_bits > 0 && num_bits <= 32) {
-        // Máscara para los bits a leer
+        // MÃ¡scara para los bits a leer
         uint32_t mascara = (1U << num_bits) - 1U;
         
         // Lee y devuelve los bits indicados
@@ -37,15 +38,16 @@ uint32_t gpio_hal_leer(GPIO_HAL_PIN_T gpio_inicial, uint8_t num_bits){
 }
 
 void gpio_hal_escribir(GPIO_HAL_PIN_T gpio_inicial, uint8_t num_bits, uint32_t valor){
-	// Verifica que los bits a escribir estén en el rango correcto
+	// Verifica que los bits a escribir estÃ©n en el rango correcto
     if (num_bits > 0 && num_bits <= 32) {
-        // Máscara para los bits a escribir
+        // MÃ¡scara para los bits a escribir
         uint32_t mascara = (1U << num_bits) - 1U;
-        
-        // Limpia los bits a escribir
-        IOPIN &= ~(mascara << gpio_inicial);
-        
-        // Escribe los nuevos valores
-        IOPIN |= (valor & mascara) << gpio_inicial;
+				uint32_t gpio_auxiliar = IOPIN;
+				
+        // Clear de los bits sobre los que vamos a escribir
+        IOCLR = (mascara << gpio_inicial);
+
+        // Set the el nuevo valor de los bits
+        IOSET = (valor & mascara) << gpio_inicial;
     }
 }
