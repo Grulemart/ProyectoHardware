ac36a6f034c84cba9a5c1f0decef7e04f6dddccf
alv02 <120688930+alv02@users.noreply.github.com>
2023-10-24 01:43:23 +0200

    Update main.c

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main() {
 	gpio_hal_sentido(GPIO_OVERFLOW, GPIO_OVERFLOW_BITS, GPIO_HAL_PIN_DIR_OUTPUT);
 	FIFO_inicializar(GPIO_OVERFLOW);
 	temporizador_drv_reloj(30,FIFO_encolar, TIMER1);
-	while (gpio_hal_leer(GPIO_OVERFLOW, GPIO_OVERFLOW) != 1){
+	while (gpio_hal_leer(GPIO_OVERFLOW, GPIO_OVERFLOW_BITS) != 1){
 	}
 	FIFO_inicializar(GPIO_OVERFLOW);
 	temporizador_drv_reloj(0,FIFO_encolar,TIMER1);
