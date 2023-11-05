8cf396a756688f9d61dbf37529b433e84889484c
alv02 <120688930+alv02@users.noreply.github.com>
2023-11-05 19:01:21 +0100

    Update int_externas_hal.h

--- int_externas_hal.h
+++ int_externas_hal.h
@@ -8,6 +8,8 @@ void iniciar_ext1(int id);
 void iniciar_ext2(int id);
 int eint1_activada(void);
 int eint2_activada(void);
+void habilitar_interrupcion_eint1(void);
+void habilitar_interrupcion_eint2(void);
 
 
 #endif
